094fa2dad6caffb5d2757b3539fffc002a5d3eef
hackeris <hackeris@qq.com>
2017-03-15 21:02:08 +0800

    add each, map, filter, group

--- main.cpp
+++ main.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
 #include "underscore.hpp"
 
+void test_each() {
+
+    std::cout << "Testing each..." << std::endl;
+
+    std::vector<int> a{1, 2, 3, 4, 5, 6};
+    _::each(a, [](const int &item) {
+        std::cout << item << " ";
+    });
+    std::cout << std::endl;
+
+    std::cout << "OK." << std::endl;
+}
+
+void test_map() {
+
+    std::cout << "Testing map..." << std::endl;
+
+    std::vector<int> a{1, 2, 3, 4, 5, 6};
+
+    using result_type = std::vector<int>;
+    auto a2 = _::map < result_type > (a, [](int item) -> int {
+        return item * 2;
+    });
+
+    _::each(a2, [](const int &item) {
+        std::cout << item << " ";
+    });
+    std::cout << std::endl;
+
+    std::cout << "OK." << std::endl;
+}
+
+void test_filter() {
+
+    std::cout << "Testing filter..." << std::endl;
+
+    std::vector<int> a{1, 2, 3, 4, 5, 6};
+    auto od = _::filter(a, [](int item) -> bool {
+        return item % 2 == 0;
+    });
+
+    _::each(od, [](const int &item) {
+        std::cout << item << " ";
+    });
+    std::cout << std::endl;
+
+    std::cout << "OK." << std::endl;
+}
+
+void test_group() {
+
+    std::cout << "Testing group..." << std::endl;
+
+    std::vector<int> a{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    auto result = _::group<int>(a, [](const int &item) -> int {
+        return item % 3;
+    });
+
+    std::cout << "OK." << std::endl;
+}
+
 int main() {
 
-
+    test_each();
+    test_map();
+    test_filter();
+    test_group();
 
     return 0;
 }
--- underscore.hpp
+++ underscore.hpp
@@ -5,4 +5,56 @@
 #ifndef UNDERSCOREPP_UNDERSCORE_HPP
 #define UNDERSCOREPP_UNDERSCORE_HPP
 
+#include <vector>
+#include <map>
+
+namespace _ {
+
+    using namespace std;
+
+    template<typename ResultContainer, typename Container, typename Function>
+    ResultContainer map(const Container &container, Function function) {
+        ResultContainer result;
+        for (const auto &item : container) {
+            result.push_back(function(item));
+        }
+        return result;
+    };
+
+    template<typename Container, typename Function>
+    Container filter(const Container &container, Function function) {
+        Container result;
+        for (const auto &item: container) {
+            if (function(item)) {
+                result.push_back(item);
+            }
+        }
+        return result;
+    };
+
+    template<typename Container, typename Function>
+    void each(const Container &container, Function function) {
+        for (const auto &item: container) {
+            function(item);
+        }
+    };
+
+    template<typename GroupKey, typename Container, typename Function>
+    std::map<GroupKey, Container> group(const Container &container, Function function) {
+        std::map<GroupKey, Container> result;
+        _::each(container, [&result, &function](const typename Container::value_type &item) {
+            GroupKey key = function(item);
+            if (result.find(key) == result.end()) {
+                Container tmpC;
+                tmpC.push_back(item);
+                result[key] = tmpC;
+            } else {
+                result[key].push_back(item);
+            }
+        });
+        return result;
+    };
+}
+
+
 #endif //UNDERSCOREPP_UNDERSCORE_HPP
